6278985874282a698f8ad7a3fccb9766ca706d8f
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-06-08 15:01:49 -0400

    Passed connection object to setInput

--- play.js
+++ play.js
@@ -2,5 +2,5 @@ const { connect } = require('./client');
 const { setupInput } = require('./input');
 
 console.log("Connecting...");
-connect();
-setupInput();
+const connection = connect();
+setupInput(connection);
