5bf1400e2b9183d6b41588957a2d7529f026f5de
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-06-08 13:44:56 -0400

    Imported connect with destructuring

--- play.js
+++ play.js
@@ -1,17 +1,4 @@
-const net = require("net");
-
-const connect = function() {
-  const conn = net.createConnection({
-    host: '192.168.2.59',
-    port: 50541
-  });
-
-  conn.setEncoding('utf8');
-  conn.on('data', (data) => {
-    console.log(data);
-  });
-  return conn;
-}
+const { connect } = require('./client');
 
 console.log("Connecting...");
 connect();
