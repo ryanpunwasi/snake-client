ad4dd2040f8875a50ee56282044ad4ae120b822e
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-06-08 15:25:47 -0400

    Refactored to use constants for ip and port

--- client.js
+++ client.js
@@ -1,9 +1,10 @@
 const net = require("net");
+const { IP, PORT } = require('./constants'); 
 
 const connect = function() {
   const conn = net.createConnection({
-    host: '192.168.2.59',
-    port: 50541
+    host: IP,
+    port: PORT
   });
   conn.on('connect', () => {
     console.log("Connected!");
